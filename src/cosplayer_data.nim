4af112417269cafa280c4f153002dff19dde23d9
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2023-03-10 22:35:03 -0300

    fix: send http code and proper headers

--- src/cosplayer_data.nim
+++ src/cosplayer_data.nim
@@ -7,34 +7,37 @@ from struct/Jury import readJuries, writeJury, readVotes, writeVote, JuryPayload
 proc setRoutes (): void =
   routes:
     get "/cosplayers":
-      resp readCosplayers()
+      resp(Http200, @[("Access-Control-Allow-Origin", "*")], readCosplayers())
 
     post "/cosplayers":
       try:
         writeCosplayer(request.body.parseJson.to CosplayerPayload)
-        resp Http200
+
+        resp(Http200, @[("Access-Control-Allow-Origin", "*")], "OK")
       except:
-        resp "Invalid JSON payload."
+        resp(Http500, "Invalid JSON payload.")
 
     get "/juries":
-      resp readJuries()
+      resp(Http200, @[("Access-Control-Allow-Origin", "*")], readJuries())
 
     post "/juries":
       try:
         writeJury(request.body.parseJson.to JuryPayload)
-        resp Http200
+
+        resp(Http200, @[("Access-Control-Allow-Origin", "*")], "OK")
       except:
-        resp "Invalid JSON payload."
+        resp(Http500, "Invalid JSON payload.")
 
     get "/votes":
-      resp readVotes()
+      resp(Http200, @[("Access-Control-Allow-Origin", "*")], readVotes())
 
     post "/votes":
       try:
         writeVote(request.body.parseJson.to Vote)
-        resp Http200
+
+        resp(Http200, @[("Access-Control-Allow-Origin", "*")], "OK")
       except:
-        resp "Invalid JSON payload."
+        resp(Http500, "Invalid JSON payload.")
 
 when isMainModule:
   setRoutes()
